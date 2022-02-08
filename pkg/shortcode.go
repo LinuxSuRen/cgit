e65f99117d6409e4723199fe61d6ff716445bfee
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2022-02-08 11:02:23 +0800

    Avoid letting the clone command panic if no gh command (#46)

--- pkg/shortcode.go
+++ pkg/shortcode.go
@@ -7,6 +7,11 @@ import (
 
 // ParseShortCode parses a short org/repo to a GitHub URL
 func ParseShortCode(args []string, output func(string) string) []string {
+	return ParseShortCodeWithProtocol(args, "https", output)
+}
+
+// ParseShortCodeWithProtocol parses a short org/repo with protocol
+func ParseShortCodeWithProtocol(args []string, protocol string, output func(string) string) []string {
 	if len(args) <= 0 {
 		return args
 	}
@@ -14,7 +19,14 @@ func ParseShortCode(args []string, output func(string) string) []string {
 		output = defaultOutput
 	}
 
-	result := []string{fmt.Sprintf("https://github.com/%s", args[0])}
+	var result []string
+	switch protocol {
+	case "https":
+		result = []string{fmt.Sprintf("https://github.com/%s", args[0])}
+	case "ssh", "git":
+		result = []string{fmt.Sprintf("git@github.com:%s.git", args[0])}
+	}
+
 	if len(args) > 1 {
 		result = append(result, args[1:]...)
 	} else {
